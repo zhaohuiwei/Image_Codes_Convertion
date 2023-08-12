86ccfdc212b8ab3dfc7d7dd2224f32d48252302c
zhaohuiwei <80906608+zhaohuiwei@users.noreply.github.com>
2023-08-12 17:06:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// 获取DOM元素
-const imageInput = document.getElementById('image-input');
-const inputImagePreview = document.getElementById('input-image-preview');
+document.addEventListener("DOMContentLoaded", () => {
+  const imageInput = document.getElementById("image-input");
+  const outputCode = document.getElementById("output-code");
 
-// 监听图片上传事件
-imageInput.addEventListener('change', handleImageUpload);
+  imageInput.addEventListener("change", (event) => {
+    const selectedImage = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", selectedImage);
 
-// 处理图片上传
-function handleImageUpload(event) {
-    const file = event.target.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            inputImagePreview.innerHTML = `<img src="${e.target.result}" alt="Uploaded Image">`;
-        };
-        reader.readAsDataURL(file);
-    } else {
-        inputImagePreview.innerHTML = '<p>Drop an image or select a file</p>';
-    }
-}
+    fetch("/generate_code", {
+      method: "POST",
+      body: formData,
+    })
+    .then(response => response.text())
+    .then(data => {
+      outputCode.value = data;
+    })
+    .catch(error => {
+      console.error("Error uploading image:", error);
+    });
+  });
+});
